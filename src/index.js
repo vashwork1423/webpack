1e2e6b4523942cf277ba9a82bb73be71227df7e9
vash.work.14 <vash.work.14@gmail.com>
2024-06-06 14:41:25 +0500

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+import './main.scss';
+import './fonts/fonts.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
